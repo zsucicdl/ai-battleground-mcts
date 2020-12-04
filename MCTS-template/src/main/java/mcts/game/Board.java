3e6f369b9884d28a1faf543653ce4e02d301d32c
andrejceraj <andrej.ceraj@gmail.com>
2020-12-04 22:12:02 +0100

    added init function

--- MCTS-template/src/main/java/mcts/game/Board.java
+++ MCTS-template/src/main/java/mcts/game/Board.java
@@ -10,7 +10,6 @@ public class Board {
     private City[] cities1;
     private City[] cities2;
 
-    private HashMap<ValuesXY, Field> positionFields;
     private HashMap<Integer, Intersection> indexIntersections;
     private HashMap<ValuesXY, Boolean> intersectionRoads;
 
@@ -21,13 +20,12 @@ public class Board {
     *  */
 
     public Board(Builder player1, Builder player2, City[] cities1, City[] cities2,
-                 HashMap<ValuesXY, Field> positionFields, HashMap<Integer, Intersection> indexIntersections,
+                 HashMap<Integer, Intersection> indexIntersections,
                  HashMap<ValuesXY, Boolean> intersectionRoads) {
         this.player1 = player1;
         this.player2 = player2;
         this.cities1 = cities1;
         this.cities2 = cities2;
-        this.positionFields = positionFields;
         this.indexIntersections = indexIntersections;
         this.intersectionRoads = intersectionRoads;
     }
@@ -64,24 +62,52 @@ public class Board {
         this.cities2 = cities2;
     }
 
-    public Board copy(){
-        // TODO implement
-        City[] newCities1 = new City[] {this.cities1[0].copy(), this.cities1[1].copy()};
-        City[] newCities2 = new City[] {this.cities2[0].copy(), this.cities2[1].copy()};
-        HashMap<ValuesXY, Field> newPositionFields = new HashMap<>(this.positionFields);
-        HashMap<Integer, Intersection> newIndexIntersections = new HashMap<>(this.indexIntersections);
-        HashMap<ValuesXY, Boolean> newIntersectionRoads = new HashMap<>(this.intersectionRoads);
-        return new Board(player1.copy(), player2.copy(), newCities1, newCities2, newPositionFields, newIndexIntersections, newIntersectionRoads);
+    public HashMap<Integer, Intersection> getIndexIntersections() {
+        return indexIntersections;
     }
 
-    public static Board initBoard(List<List<Integer>> intersectionToIntersection, List<List<ValuesXY>> intersectionToField){
-        HashMap<ValuesXY, Field> positionFields = new HashMap<>();
+    public void setIndexIntersections(HashMap<Integer, Intersection> indexIntersections) {
+        this.indexIntersections = indexIntersections;
+    }
+
+    public HashMap<ValuesXY, Boolean> getIntersectionRoads() {
+        return intersectionRoads;
+    }
+
+    public void setIntersectionRoads(HashMap<ValuesXY, Boolean> intersectionRoads) {
+        this.intersectionRoads = intersectionRoads;
+    }
+
+    public Board copy(){
+        City[] newCities1 = new City[] {this.cities1[0].copy(), this.cities1[1].copy()};
+        City[] newCities2 = new City[] {this.cities2[0].copy(), this.cities2[1].copy()};
+
+        HashMap<ValuesXY, Boolean> newIntersectionRoads = new HashMap<>(this.intersectionRoads);
+        return new Board(player1.copy(), player2.copy(), newCities1, newCities2, this.indexIntersections, newIntersectionRoads);
+    }
+
+    public static Board initBoard(List<List<Integer>> intersectionToIntersection, List<List<Field>> intersectionToField){
         HashMap<Integer, Intersection> indexIntersections = new HashMap<>();
         HashMap<ValuesXY, Boolean> intersectionRoads = new HashMap<>();
 
-        int n = intersectionToField.size();
-        for(int i = 0; i < n; i++){
+        int n = intersectionToIntersection.size();
 
+        for(int i = 0; i < n; i++){
+            if(!indexIntersections.containsKey(i)){
+                indexIntersections.put(i, new Intersection());
+            }
+            Intersection currentIntersection = indexIntersections.get(i);
+            for(Integer index : intersectionToIntersection.get(i)){
+                if(!indexIntersections.containsKey(index)){
+                    indexIntersections.put(index, new Intersection());
+                }
+                currentIntersection.addAdjacentIntersection(indexIntersections.get(index));
+            }
+
+            for(Field field : intersectionToField.get(i)){
+                currentIntersection.addAdjacentField(field);
+            }
         }
+        return new Board(new Builder(1), new Builder(2), new City[2], new City[2], indexIntersections, intersectionRoads);
     }
 }
--- MCTS-template/src/main/java/mcts/game/Intersection.java
+++ MCTS-template/src/main/java/mcts/game/Intersection.java
@@ -1,18 +1,20 @@
 package mcts.game;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Intersection {
     private List<Field> adjacentFields;
-    private List<Intersection> adjacentIntersections;
+    private Set<Intersection> adjacentIntersections;
 
     public Intersection() {
         adjacentFields = new ArrayList<>();
-        adjacentIntersections = new ArrayList<>();
+        adjacentIntersections = new HashSet<>();
     }
 
-    public Intersection(List<Field> adjacentFields, List<Intersection> adjacentIntersections) {
+    public Intersection(List<Field> adjacentFields, Set<Intersection> adjacentIntersections) {
         this.adjacentFields = adjacentFields;
         this.adjacentIntersections = adjacentIntersections;
     }
@@ -25,11 +27,11 @@ public class Intersection {
         this.adjacentFields = adjacentFields;
     }
 
-    public List<Intersection> getAdjacentIntersections() {
+    public Set<Intersection> getAdjacentIntersections() {
         return adjacentIntersections;
     }
 
-    public void setAdjacentIntersections(List<Intersection> adjacentIntersections) {
+    public void setAdjacentIntersections(Set<Intersection> adjacentIntersections) {
         this.adjacentIntersections = adjacentIntersections;
     }
 
@@ -39,5 +41,6 @@ public class Intersection {
 
     public void addAdjacentIntersection(Intersection intersection){
         this.adjacentIntersections.add(intersection);
+        intersection.addAdjacentIntersection(this);
     }
 }
